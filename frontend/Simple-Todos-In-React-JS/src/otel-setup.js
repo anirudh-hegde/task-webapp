36516cc306631a7974d9ede2762298324f8930ff
anirudh-hegde <hegdeanirudh2003@gmail.com>
2025-06-16 10:28:20 +0530

    changes done in frontend and .gitignore

--- frontend/Simple-Todos-In-React-JS/src/otel-setup.js
+++ frontend/Simple-Todos-In-React-JS/src/otel-setup.js
@@ -1,7 +1,10 @@
-import { WebTracerProvider } from '@opentelemetry/sdk-trace-web'
-import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base'
-import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch'
-import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load'
+import {WebTracerProvider} from '@opentelemetry/sdk-trace-web'
+import {
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} from '@opentelemetry/sdk-trace-base'
+import {FetchInstrumentation} from '@opentelemetry/instrumentation-fetch'
+import {DocumentLoadInstrumentation} from '@opentelemetry/instrumentation-document-load'
 
 const provider = new WebTracerProvider()
 provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()))
