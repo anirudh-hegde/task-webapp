ebb7cee973c2413a4751da02de5fe4f40f78ab5b
Anirudh Hegde <hegdeanirudh2003@gmail.com>
2025-06-17 21:21:40 +0530

    Update index.js

--- frontend/Simple-Todos-In-React-JS/src/index.js
+++ frontend/Simple-Todos-In-React-JS/src/index.js
@@ -1,11 +1,17 @@
-import './otel-setup'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import './otel-setup'; 
 
-ReactDOM.render(
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+// Initialize your OpenTelemetry instrumentation
+
+const container = document.getElementById('root');
+const root = createRoot(container); // Create a root.
+
+// Render the app using the new root API
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+  </React.StrictMode>
+);
